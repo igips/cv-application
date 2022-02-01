86364b78fedb44ea3d866db5fc1c73b3260aa4fc
igips <igor.szepietowski@gmail.com>
2022-02-01 16:09:57 +0100

    Create basic form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,33 @@
+import React from "react";
+import "./styles/App.css";
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+
+  render() {
+    return (
+      <div className="app">
+        <header>CV Builder</header>
+
+        
+
+      </div>
+
+
+
+
+    );
+  }
+
+}
+
+
+
 
 
 export default App;
