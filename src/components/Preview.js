79a08c6fee762d72bb0b669955f70cbb0a8b59cf
igips <igor.szepietowski@gmail.com>
2022-02-06 15:05:34 +0100

    Design CV

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -37,9 +37,60 @@ class Preview extends React.Component {
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempus imperdiet nisl
 							sed vestibulum. Donec gravida, nulla eget blandit fermentum, mauris nisi rutrum libero, ac
 							pharetra erat est sit amet tellus. Quisque fermentum dolor a interdum fermentum. Maecenas
-							vehicula ac ipsum nec gravida. Integer quis porta turpis. Aenean et metus.¯
+							vehicula ac ipsum nec gravida. Integer quis porta turpis. Aenean et metus.
 						</article>
-                        <p>Experience</p>
+						<p>Experience</p>
+						<div className="exEdDiv">
+							<div className="exEdRow">
+								<div className="exEdRowLeft">2018 - Present</div>
+								<div className="exEdRowRight">
+									Junior Web Developer <br />
+									<span>Twitter Inc, &nbsp; Dupeloria</span>
+								</div>
+							</div>
+							<div className="exEdRow">
+								<div className="exEdRowLeft">2015 - 2018</div>
+								<div className="exEdRowRight">
+									Junior Web Developer <br />
+									<span>Facebook Inc, &nbsp; Ontario</span>
+								</div>
+							</div>
+                            <div className="exEdRow">
+								<div className="exEdRowLeft">2012 - 2015</div>
+								<div className="exEdRowRight">
+									Junior Web Developer <br />
+									<span>Google Inc, &nbsp; London</span><br />
+                                    <span>Working on sram szczam fam</span>
+								</div>
+							</div>
+						</div>
+                        <p>Education</p>
+                        <div className="exEdDiv">
+							<div className="exEdRow">
+								<div className="exEdRowLeft">2008 - 2010</div>
+								<div className="exEdRowRight">
+									University of Michigan <br />
+									<span>Field of study: &nbsp; History Of Art</span><br />
+                                    <span>Degree: &nbsp; Master</span>
+								</div>
+							</div>
+							<div className="exEdRow">
+								<div className="exEdRowLeft">2005 - 2008</div>
+								<div className="exEdRowRight">
+									University of Life <br />
+									<span>Field of study: &nbsp; Smoking Weed</span><br />
+                                    <span>Degree: &nbsp; Master</span>
+								</div>
+							</div>
+                            <div className="exEdRow">
+								<div className="exEdRowLeft">2003 - 2005</div>
+								<div className="exEdRowRight">
+									University of Street <br />
+									<span>Field of study: &nbsp; Hustling</span><br />
+                                    <span>Degree: &nbsp; Bachelor</span>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</>
