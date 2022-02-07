348d154ef7225c257f7889c49d33bd6185a10ae6
igips <igor.szepietowski@gmail.com>
2022-02-07 20:31:51 +0100

    Refactor using functional components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,40 +1,48 @@
 import React from "react";
 import "../styles/Button.css";
 
-class Button extends React.Component {
-	render() {
-		let button;
+function Button(props) {
+	let button;
 
-		if (this.props.type === "delete") {
-			button = (
-				<div className="buttonDiv">
-					<button onClick={this.props.click} className="delButton">
-						Delete
-					</button>
-				</div>
-			);
-		} else if (this.props.type === "add") {
-			button = (
-				<div className="buttonDiv">
-					<button onClick={this.props.click} className="addButton">
-						Add
-					</button>
-				</div>
-			);
-		} else if (this.props.type === "preview") {
-			button = <button onClick={this.props.prev}  className="previewButton">Preview</button>;
-		} else if (this.props.type === "reset") {
-			button = <button onClick={this.props.res} className="resetButton">Reset</button>;
-
-		} else if (this.props.type === "edit") {
-			button = <button onClick={this.props.prev}  className="editButton">Edit</button>;
-
-		} else if (this.props.type === "pdf") {
-			button = <button   className="pdfButton">Save PDF</button>;
-		}
-
-		return button;
+	if (props.type === "delete") {
+		button = (
+			<div className="buttonDiv">
+				<button onClick={props.click} className="delButton">
+					Delete
+				</button>
+			</div>
+		);
+	} else if (props.type === "add") {
+		button = (
+			<div className="buttonDiv">
+				<button onClick={props.click} className="addButton">
+					Add
+				</button>
+			</div>
+		);
+	} else if (props.type === "preview") {
+		button = (
+			<button onClick={props.prev} className="previewButton">
+				Preview
+			</button>
+		);
+	} else if (props.type === "reset") {
+		button = (
+			<button onClick={props.res} className="resetButton">
+				Reset
+			</button>
+		);
+	} else if (props.type === "edit") {
+		button = (
+			<button onClick={props.prev} className="editButton">
+				Edit
+			</button>
+		);
+	} else if (props.type === "pdf") {
+		button = <button className="pdfButton">Save PDF</button>;
 	}
+
+	return button;
 }
 
 export default Button;
