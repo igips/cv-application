fd737442a560dd7d7fc02955949d67f8606393e5
igips <igor.szepietowski@gmail.com>
2022-02-07 20:34:20 +0100

    Refactor from class components to functional components

--- src/components/PrevRes.js
+++ src/components/PrevRes.js
@@ -1,19 +1,13 @@
 import React from "react";
 import Button from "./Button";
 
-class PrevRes extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<div className="buttonDiv">
-				<Button prev={this.props.prev}  type="preview"></Button>
-				<Button res={this.props.res} type="reset"></Button>
-			</div>
-		);
-	}
+function PrevRes(props) {
+	return (
+		<div className="buttonDiv">
+			<Button prev={props.prev} type="preview"></Button>
+			<Button res={props.res} type="reset"></Button>
+		</div>
+	);
 }
 
 export default PrevRes;
