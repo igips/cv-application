7d9356e5d57b6bfbbe74283e5970670e81976519
igips <igor.szepietowski@gmail.com>
2022-02-07 20:57:40 +0100

    Rewrite using functional components

--- src/App.js
+++ src/App.js
@@ -2,26 +2,20 @@ import React from "react";
 import Form from "./components/Form";
 import "./styles/App.css";
 
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+function App() {
+	return (
+		<div className="app">
+			<header>CV Builder</header>
 
-	render() {
-		return (
-			<div className="app">
-				<header>CV Builder</header>
+			<Form></Form>
 
-				<Form></Form>
-
-				<footer>
-					<a href="https://github.com/igips" target="_blank" rel="noreferrer">
-						<i className="fab fa-github"></i>
-					</a>
-				</footer>
-			</div>
-		);
-	}
+			<footer>
+				<a href="https://github.com/igips" target="_blank" rel="noreferrer">
+					<i className="fab fa-github"></i>
+				</a>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
