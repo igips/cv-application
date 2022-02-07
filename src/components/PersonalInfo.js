965ae380a0fe0192c73dc672bf324c8374561804
igips <igor.szepietowski@gmail.com>
2022-02-07 20:56:16 +0100

    Rewrite using functional components

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,42 +1,18 @@
 import React from "react";
 import Input from "./Input";
 
-class PersonalInfo extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<div className="formLine">
-				<Input val={this.props.val.fullName} change={this.props.change} name="fullName" for="Full Name"></Input>
-				<Input val={this.props.val.title} change={this.props.change} name="title" for="Title"></Input>
-				<Input change={this.props.change} name="photo" for="Photo" input="file"></Input>
-				<Input val={this.props.val.address} change={this.props.change} name="address" for="Address"></Input>
-				<Input
-					val={this.props.val.number}
-					change={this.props.change}
-					name="number"
-					for="Phone Number"
-					input="num"
-				></Input>
-				<Input
-					val={this.props.val.email}
-					change={this.props.change}
-					name="email"
-					for="Email"
-					input="email"
-				></Input>
-				<Input
-					val={this.props.val.about}
-					change={this.props.change}
-					name="about"
-					for="About You"
-					input="textarea"
-				></Input>
-			</div>
-		);
-	}
+function PersonalInfo(props) {
+	return (
+		<div className="formLine">
+			<Input val={props.val.fullName} change={props.change} name="fullName" for="Full Name"></Input>
+			<Input val={props.val.title} change={props.change} name="title" for="Title"></Input>
+			<Input change={props.change} name="photo" for="Photo" input="file"></Input>
+			<Input val={props.val.address} change={props.change} name="address" for="Address"></Input>
+			<Input val={props.val.number} change={props.change} name="number" for="Phone Number" input="num"></Input>
+			<Input val={props.val.email} change={props.change} name="email" for="Email" input="email"></Input>
+			<Input val={props.val.about} change={props.change} name="about" for="About You" input="textarea"></Input>
+		</div>
+	);
 }
 
 export default PersonalInfo;
